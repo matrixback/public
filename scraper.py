d6ac1644be52c797867c12b9bfd2ed5dba835bdf
matrix <matrix32768@163.com>
2022-12-01 19:47:14 +0800

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -6,39 +6,57 @@ from urllib.parse import urlparse
 from bs4 import BeautifulSoup
 from bs4.element import NavigableString
 
+# 控制一个url只请求一次
 visited_urls = set()
+# 包含单词最多的页面
 longest_page = ''
 longest = 0
 # words_cnt for statistic common 50 words
+# 统计出现次数最多的50个单词
 words_cnt = defaultdict(int)
 
 
 def scraper(url, resp):
+    """
+    此函数从当前爬取的页面 resp 中提取有效的 url，供爬虫再次爬取
+    url: 当前爬取页面的 url
+    resp: 当前爬取页面的 response，一个 html 文件
+    return: 有效的 url list
+    """
+    # 在函数中使用函数外定义的变量
     global longest, longest_page
+    # 先将已爬到数据的 URL 保存到文件中，后面统计数据时使用
     with open('url.txt', 'a+') as f:
         f.write(f'{url}\n')
 
     try:
+        # 如果返回的数据内容为空，则直接返回空list
         if (not resp
             or not resp.raw_response
             or not resp.raw_response.content):
             return []
+        
+        # 获取当前页面包含的单词长度
         length = get_html_text_words_cnt(resp.raw_response.content)
     except Exception as e:
         print(e)
         return []
-
+   
+    # 将 url 以及页面单词的长度 length 保存起来，后面统计用
     with open('words.txt', 'a+') as f:
         print(f'{url},{length}')
         f.write(f'{url},{length}\n')
+        # 更新当前的最大长度页面
         if length > longest:
             longest = length
             longest_page = url
             print(f'longest url {longest_page}, words: {longest}')
 
     save_top_50_words()
-
+    
+    # 从当前页面提取所有的 url
     links = extract_next_links(url, resp)
+    # 对于提取的url，将有效的 url 添加到 invalid_links 返回
     invalid_links = list()
     for link in links:
         if not is_valid(link):
@@ -50,6 +68,10 @@ def scraper(url, resp):
 
 
 def extract_next_links(url, resp):
+    """
+    此函数解析 resp html 页面，获取所有未爬取的 url
+    返回 url list
+    """
     # Implementation required.
     # url: the URL that was used to get the page
     # resp.url: the actual url of the page
@@ -59,14 +81,22 @@ def extract_next_links(url, resp):
     #         resp.raw_response.url: the url, again
     #         resp.raw_response.content: the content of the page!
     # Return a list with the hyperlinks (as strings) scrapped from resp.raw_response.content
+    
+    
+    # 如果 resp 的状态码不为200，则页面无效，直接返回空list
     if resp.status != 200:
         return list()
 
+    # 通过 beautifulsoup 解析当前的 html 页面
     soup = BeautifulSoup(resp.raw_response.content, 'html.parser')
     links = list()
+    # parse 当前url的信息
     parent_url_parsed = urlparse(url)
+    # 从html 页面提取所有的 a tag
     for link in soup.find_all('a'):
+        # 获取 a tag 的href 属性，即 url
         url = link.get('href')
+        # 解析当前的 url，如果缺少必要信息，则从父 url 中获取
         parsed = urlparse(url)
         if parsed.scheme:
             scheme = parsed.scheme
@@ -81,7 +111,7 @@ def extract_next_links(url, resp):
             url = scheme + "://" + hostname + parsed.path
         except Exception as e:
             continue
-
+        # 如果未爬取，则添加到 links 中，返回
         if url not in visited_urls:
             visited_urls.add(url)
             links.append(url)
@@ -90,14 +120,19 @@ def extract_next_links(url, resp):
 
 
 def is_valid(url):
+    """
+    此函数判断一个 url 是否有效，返回 bool 类型
+    """
     # Decide whether to crawl this url or not.
     # If you decide to crawl it, return True; otherwise return False.
     # There are already some conditions that return False.
     try:
+        # 解析 url ，获取 url 信息
         parsed = urlparse(url)
         if parsed.scheme not in set(["http", "https"]):
             return False
-
+        
+        # 如果 url.path 以下面这些结尾，则为无效 url
         if re.match(
                 r".*\.(css|js|bmp|gif|jpe?g|ico"
                 + r"|png|tiff?|mid|mp2|mp3|mp4"
@@ -108,11 +143,17 @@ def is_valid(url):
                 + r"|thmx|mso|arff|rtf|jar|csv"
                 + r"|rm|smil|wmv|swf|wma|zip|rar|gz)$", parsed.path.lower()):
             return False
-
+        
+        # 判断特殊的url
         path = parsed.hostname + parsed.path.lower()
         if path.startswith('today.uci.edu/department/information_computer_sciences/'):
             return True
-
+        
+        # 如果 path 符合的正则表达式，为为有效的 url
+        # .*\.(ics.uci.edu/|cs.uci.edu/|informatics.uci.edu/|stat.uci.edu/).*
+        # .* 表示0个或多个字符
+        # | 表示或
+        # xxx.(ics.uci.edu/ 或者 cs.uci.edu 等).xxx 就是合格的
         if re.match(
                 r".*\.(ics.uci.edu/"
                 + r"|cs.uci.edu/"
